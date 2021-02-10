43fc46921ef0049c7f1e5d592ed151935f94f108
Mathew Trivett <matt.trivett@gmail.com>
2021-02-10 15:59:36 +0000

    Updates my now page

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,24 @@
 import Typography from "typography"
 import theme from 'typography-theme-github';
 
+theme.overrideStyles = ({rhythm}, options) => ({
+  'h1,h2,h3': {
+    border: 'none'
+  },
+  'blockquote': {
+    margin: 0,
+    paddingLeft: '1rem',
+    borderLeft: '5px solid grey'
+  },
+  
+})
+
 const typography = new Typography({
   ...theme,
   headerColor: 'var(--primary)',
   baseFontSize: '18px',
-  includeNormalize: true
+  includeNormalize: true,
+  blockMarginBottom: '1.75rem'
 });
 
 
